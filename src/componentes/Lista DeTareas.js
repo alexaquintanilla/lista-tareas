e685db473c91fc046d3872d404c487ca0274063e
Lady201 <quintanillarojasleydymassiell@gmail.com>
2024-05-09 21:56:50 -0500

    update

--- src/componentes/Lista DeTareas.js
+++ src/componentes/Lista DeTareas.js
@@ -5,13 +5,40 @@ import '../hojas-de-estilo/ListaDeTareas.css'
 
 
 function ListaDeTareas() {
-
+  
    const[tareas, setTareas] = useState([]);
 
-   const agregarTarea = tarea => {
-    console.log("Tarea agregada");
+   const agregarTarea= tarea => { 
     console.log(tarea);
-   }
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+   };
+
+  //  const agregarTarea = (tarea) => {
+  //   console.log("Tarea agregada");
+  //   console.log(tarea);
+
+  //   const nuevasTareas = [...tareas, tarea]
+  //   console.log(nuevasTareas);
+    
+  //   setTareas(nuevasTareas)
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
+
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  };
 
     return (
         <>
@@ -20,9 +47,12 @@ function ListaDeTareas() {
               {
                 tareas.map((tarea) =>
                    <Tarea 
+                   key={tarea.id}
+                   id={tarea.id}
                    texto={tarea.texto}
-                   completado={tarea.completado}
-                   />
+                   completada={tarea.completada}
+                   completarTarea={completarTarea}
+                   eliminarTarea={eliminarTarea} />
                 )
               }
           </div>
